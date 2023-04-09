a9e931e4d0b1b2d8c458429837dec25a3b4a1196
unknown <mehekjain28@gmail.com>
2023-04-09 10:21:01 +0530

    react complete app

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,33 +1,34 @@
-import React, { useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
+import { NavLink } from "react-router-dom";
 
 const Login = () => {
-//   const [username, setUsername]=useState('')
-//   const [pass, setpass]=useState('')
-//   const [email, setemail]=useState('')
+  //   const [username, setUsername]=useState('')
+  //   const [pass, setpass]=useState('')
+  //   const [email, setemail]=useState('')
 
-// async function submit(){
-//   email.preventDefault();
-//   try{
-//     await axios.post("http://localhost:8000/",{
-//       username,email,pass
-//     }).then(res=>{
-//       if(res.data=="exist"){
-//         history("/home",{state:{id:email}})
-//       }else if(res.data="notexist"){
-//         alert("User have not registered")
-//       }
-//     })
-//   }catch(e){
-//     console.log(e)
-//   }
-// }
+  // async function submit(){
+  //   email.preventDefault();
+  //   try{
+  //     await axios.post("http://localhost:8000/",{
+  //       username,email,pass
+  //     }).then(res=>{
+  //       if(res.data=="exist"){
+  //         history("/home",{state:{id:email}})
+  //       }else if(res.data="notexist"){
+  //         alert("User have not registered")
+  //       }
+  //     })
+  //   }catch(e){
+  //     console.log(e)
+  //   }
+  // }
 
-  
+
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8" style={{alignContent:'center', alignItems:'center'}}>
-      <div className='flex flex-col justify-center' style={{height:'500px',width:'500px',alignItems:'center',border:'2px solid #ef4444',borderRadius:'25px', background:'#f57f58'}}>
+    <div className="min-h-screen bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8" style={{ alignContent: 'center', alignItems: 'center' }}>
+      <div className='flex flex-col justify-center' style={{ height: '500px', width: '500px', alignItems: 'center', border: '2px solid #ef4444', borderRadius: '25px', background: '#f57f58' }}>
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
-          <p className='items-center mt-8 ml-44 log text-5xl' style={{transform:'translateX(-13px)'}}><strong>KidSecure</strong></p>
+          <p className='items-center mt-8 ml-44 log text-5xl' style={{ transform: 'translateX(-13px)' }}><strong>KidSecure</strong></p>
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900 ">
             Sign in to your account
           </h2>
@@ -75,12 +76,12 @@ const Login = () => {
               </div>
 
               <div>
-                <button
+                <NavLink to="/parent"> <button
                   type="submit"
                   className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
                 >
                   Sign in
-                </button>
+                </button></NavLink>
               </div>
             </form>
           </div>
--- src/Components/Signin.js
+++ src/Components/Signin.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useState } from 'react';
+import { NavLink } from "react-router-dom";
 
 const Signin = () => {
   const [username, setUsername] = useState('');
@@ -11,58 +12,58 @@ const Signin = () => {
   };
 
   return (
-    <div className='bg-gray-100 h-screen w-screen flex' style={{width:'100%', alignItems:'center',justifyContent:'center',alignContent:'center'}}>
-      <div style={{border:'2px solid #ef4444',height:'550px',width:'35vw',borderRadius:'25px',backgroundColor:'#f57f58'}}>
-      <form onSubmit={handleSubmit} className="max-w-md mx-auto">        
-        <p className='justify-content ml-44 log text-5xl' style={{paddingTop:'40px',transform: "translateX(-20px)",}}><strong>KidSecure</strong></p>
-        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900 mb-12">
-          Register for your account
-        </h2>
-        <div className="mb-4">
-          <label htmlFor="username" className="block text-gray-700 font-bold mb-2">
-            Username
-          </label>
-          <input
-            type="text"
-            id="username"
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            value={username}
-            onChange={(event) => setUsername(event.target.value)}
-          />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="email" className="block text-gray-700 font-bold mb-2">
-            Email
-          </label>
-          <input
-            type="email"
-            id="email"
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            value={email}
-            onChange={(event) => setEmail(event.target.value)}
-          />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="password" className="block text-gray-700 font-bold mb-2">
-            Password
-          </label>
-          <input
-            type="password"
-            id="password"
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-          />
-        </div>
-        <div className="flex items-center justify-between">
-          <button
-            type="submit"
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-          >
-            Register
-          </button>
-        </div>
-      </form>
+    <div className='bg-gray-100 h-screen w-screen flex' style={{ width: '100%', alignItems: 'center', justifyContent: 'center', alignContent: 'center' }}>
+      <div style={{ border: '2px solid #ef4444', height: '550px', width: '35vw', borderRadius: '25px', backgroundColor: '#f57f58' }}>
+        <form onSubmit={handleSubmit} className="max-w-md mx-auto">
+          <p className='justify-content ml-44 log text-5xl' style={{ paddingTop: '40px', transform: "translateX(-20px)", }}><strong>KidSecure</strong></p>
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900 mb-12">
+            Register for your account
+          </h2>
+          <div className="mb-4">
+            <label htmlFor="username" className="block text-gray-700 font-bold mb-2">
+              Username
+            </label>
+            <input
+              type="text"
+              id="username"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              value={username}
+              onChange={(event) => setUsername(event.target.value)}
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="email" className="block text-gray-700 font-bold mb-2">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="password" className="block text-gray-700 font-bold mb-2">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <NavLink to="/parent">  <button
+              type="submit"
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            >
+              Register
+            </button></NavLink>
+          </div>
+        </form>
       </div>
     </div>
   );
